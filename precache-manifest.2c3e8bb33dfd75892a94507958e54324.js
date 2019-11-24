4a4c17bf92b5bd31aeb6c21af7ff47f838c9972b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-24 16:37:48 +0000

    Deploy Dmoleiro/dsantos to github.com/Dmoleiro/dsantos.git:gh-pages

---
+++ precache-manifest.2c3e8bb33dfd75892a94507958e54324.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfae7a0b085312b216ca6246eb3d958",
+    "url": "/dsantos/index.html"
+  },
+  {
+    "revision": "8050341342b10d497314",
+    "url": "/dsantos/static/css/main.0bda27e0.chunk.css"
+  },
+  {
+    "revision": "a7f63ebbab829bfe857d",
+    "url": "/dsantos/static/js/2.95dc976b.chunk.js"
+  },
+  {
+    "revision": "8050341342b10d497314",
+    "url": "/dsantos/static/js/main.a3b3a265.chunk.js"
+  },
+  {
+    "revision": "d24c94426fdc2fc13abb",
+    "url": "/dsantos/static/js/runtime-main.4bff06c5.js"
+  }
+]);
